acc389fe90513be0d749f0f0d2081bf7690cc1fb
ttelcl <9429819+ttelcl@users.noreply.github.com>
2025-10-24 22:49:38 +0300

    Stub for "vsdeps scan"

---
+++ vsdeps/AppScan.fs
@@ -0,0 +1,69 @@
+﻿module AppScan
+
+open System
+open System.IO
+
+open Newtonsoft.Json
+
+open XsvLib
+
+open Lcl.VsUtilities.Solutions
+
+open ColorPrint
+open CommonTools
+
+type private ScanOptions = {
+  Tag: string
+  Root: string
+}
+
+let private runScan o =
+  cp "\frNYI\f0."
+  1
+
+let run args =
+  let rec parsemore o args =
+    match args with
+    | "-v" :: rest ->
+      verbose <- true
+      rest |> parsemore o
+    | "-h" :: _
+    | "-help" :: _
+    | "--help" :: _ ->
+      None
+    | "-d" :: directory :: rest ->
+      if directory |> Directory.Exists |> not then
+        cp $"\frError: directory not found: \fo{directory}\f0."
+        cp ""
+        None
+      else
+        rest |> parsemore {o with Root = directory |> Path.GetFullPath}
+    | "-tag" :: tag :: rest ->
+      rest |> parsemore {o with Tag = tag}
+    | [] ->
+      if o.Root |> String.IsNullOrEmpty then
+        cp "\frNo root directory specified\f0 (\fg-d\f0 option)"
+        cp ""
+        None
+      else
+        if o.Tag |> String.IsNullOrEmpty then
+          o |> Some
+        else
+          let tag = o.Root |> Path.GetFileName
+          {o with Tag = tag} |> Some
+    | x :: _ ->
+      cp $"\frUnrecognized argument \fy{x}\f0."
+      cp ""
+      None
+  let oo = args |> parsemore {
+    Tag = null
+    Root = null
+  }
+  match oo with
+    | None ->
+      Usage.usage "scan"
+      1
+    | Some(o) ->
+      o |> runScan
+  
+
--- vsdeps/Usage.fs
+++ vsdeps/Usage.fs
@@ -5,16 +5,27 @@ open CommonTools
 open ColorPrint
 
 let usage focus =
-  cp "\fovsdeps\f0: Tool to analyze Visual Studio solutions and project dependencies in them."
-  cp ""
-  cp "\fovsdeps \fysln \f0[\fg-f \fcfile.sln\f0|\fg-sln\f0] [\fg-tag \fc<tag>\f0]"
-  cp "  Analyzes a solution file and the projects in it, emitting the results as data files."
-  cp "  \fg-f \fcfile.sln\f0    The solution file to analyze"
-  cp "  \fg-sln\fx\f0           Use the first solution file found in the current folder"
-  cp "                \fx\fx\fx or an ancestor folder."
-  cp "  \fg-tag \fc<tag>\f0     If given: include the \fctag\f0 in the output file names."
-  cp "                \fx\fx\fx (preceded and followed by a '\fo.\f0')"
-  cp ""
+  if focus = "" then
+    cp "\fovsdeps\f0: Tool to analyze Visual Studio solutions and project dependencies in them."
+    cp ""
+  if focus = "" || focus = "sln" then
+    cp "\fovsdeps \fysln \f0[\fg-f \fcfile.sln\f0|\fg-sln\f0] [\fg-tag \fc<tag>\f0]"
+    cp "  Analyzes a solution file and the projects in it, emitting the results as data files."
+    cp "  \fg-f \fcfile.sln\f0    The solution file to analyze"
+    cp "  \fg-sln\fx\f0           Use the first solution file found in the current folder"
+    cp "                \fx\fx\fx or an ancestor folder."
+    cp "  \fg-tag \fc<tag>\f0     If given: include the \fctag\f0 in the output file names."
+    cp "                \fx\fx\fx (preceded and followed by a '\fo.\f0')"
+    cp ""
+  if focus = "" || focus = "scan" then
+    cp "\fovsdeps \fyscan \fg-d \fc<directory>\f0 [\fg-tag \fc<tag>\f0]"
+    cp "  Look for \fo*.sln\f0, \fo*.csproj\f0 and \fo*.fsproj\f0 file in the specified"
+    cp "  directory and its descendent directories, and map relations between solution"
+    cp "  and projects, and in between projects."
+    cp "  \fg-d \fc<root>\f0      The root of the directory tree to scan"
+    cp "  \fg-tag \fc<tag>\f0     Used to construct output file names. " 
+    cp "  \fx     \fx     \fx     Defaults to the final segment of the root directory"
+    cp ""
   cp "\fg-v               \f0Verbose mode"
 
 
